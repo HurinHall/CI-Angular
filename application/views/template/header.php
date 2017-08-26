dcbb241a2d60a187eaa0577c6d92350e2b16f28c
HurinHall <hurin@live.ca>
2017-08-26 16:40:58 +1200

    init repository

---
+++ application/views/template/header.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+		<link rel="stylesheet" href="<?=base_url('css/bootstrap.min.css');?>" />
+		<script src="<?=base_url('js/jquery-3.2.1.min.js')?>"></script>
+		<script src="<?=base_url('js/popper.min.js')?>"></script>
+		<script src="<?=base_url('js/bootstrap.min.js')?>"></script>
+		<script src="<?=base_url('js/angular.min.js')?>"></script>
+	</head>
+	<body>
